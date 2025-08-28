269db710863b822b04b21495107f12a5b4b782da
Emanuele <emanueleoa2009@gmail.com>
2025-08-28 10:09:16 -0300

    Delete EX4.JOGADORES.py

--- EX4.JOGADORES.py
+++
@@ -1,6 +0,0 @@
-numjogadores= int(input("Digite o número de jogadores: "))
-mediaaltura= 0
-for i in range(numjogadores):
-    altura= int(input ("Digite a altura do jogador em cm: "))
-mediaaltura= (altura)+ (mediaaltura)/ numjogadores
-print("A altura média dos jogadores é:", mediaaltura)
