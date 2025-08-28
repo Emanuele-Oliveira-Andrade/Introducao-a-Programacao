9c6f0d066a9bdbcf7d7a48c7e2ac327f1fa73ca4
Emanuele <emanueleoa2009@gmail.com>
2025-08-28 09:58:09 -0300

    Update EX9- Numeros primos.py
    finalizado

--- EX9- Numeros primos.py
+++ EX9- Numeros primos.py
@@ -1,6 +1,9 @@
-numero= int(input("Digite um número: "))
+numero= int(input("Digite um número: " ))
 cont=0
 for i in range(1, numero):
-    numero%i==0
+    numero%2==0
     cont+=1
-if numero 
+if numero%i!=0:
+    print("O número ", numero, "não é primo.")
+else:
+    print("O número", numero, "é primo.")
