9f5c00a6c43ebb87eb3ef27b84dca0c3583e5294
Emanuele <emanueleoa2009@gmail.com>
2025-09-05 15:16:43 -0300

    Update EX6.LACOS_WHILE.py

--- EX6.LACOS_WHILE.py
+++ EX6.LACOS_WHILE.py
@@ -7,7 +7,6 @@ while i<10:
     N1= float(input("Digite a nota 1: "))
     N2= float(input("Digite a nota 2: "))
     N3= float(input("Digite a nota 3: "))
-    i+=1
     medAluno= (N1+N2+N3)/3
     if medAluno>maiorMedia:
         maiorMedia=medAluno
@@ -16,7 +15,9 @@ while i<10:
     if medAluno>=6:
         alunosAp+=1
     else: alunosRep+=1
+    i+=1
 print("A maior média é: ", maiorMedia)
 print("A menor média é ", menorMedia)
 print("A quantidade de alunos aprovados é: ", alunosAp)
+
 print("A quantidade de alunos reprovados é ", alunosRep)
