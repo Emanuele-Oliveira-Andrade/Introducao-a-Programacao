fee03c5a7688b18ccd83159a67ad0b1241122fb3
Emanuele <emanueleoa2009@gmail.com>
2025-09-11 09:55:44 -0300

    Update EX7.LACOS_WHILE.py

--- EX7.LACOS_WHILE.py
+++ EX7.LACOS_WHILE.py
@@ -1,5 +1,8 @@
 Thanos=1.50
 Miranha=1.10
-while Miranha>Thanos:
+anos= 0
+while Miranha<Thanos:
         Thanos+=0.02
         Miranha+=0.03
+        anos+=1
+print("Vai demorar", anos, "para o Homem Aranha ser maior que o Thanos.")
