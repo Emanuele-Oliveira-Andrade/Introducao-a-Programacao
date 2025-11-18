e5326d2771010b7c95f0c4da0e51251e254d9b5f
Emanuele <emanueleoa2009@gmail.com>
2025-11-18 07:57:47 -0300

    Update QUESTÃO- ROBÔ.py

--- QUESTÃO- ROBÔ.py
+++ QUESTÃO- ROBÔ.py
@@ -1,9 +1,9 @@
 bateria= int(input("Digite o nível de bateria: "))
 temperatura= int(input("Digite a temperatura do ambiente: "))
 umidade= int(input("Digite a umidade do solo: "))
-modo= input("Digite o modo de operação (plantio, colheita ou irrigação): ")
+modo= input("Digite o modo de operação (plantio, colheita ou irrigação): ").upper()
 
-#REGRAS DA BATERIA
+
 if bateria < 20:
     print("Bateria muito baixa! Retorne imediatamente para a base.")
 if bateria>=20:
@@ -12,25 +12,26 @@ if bateria>=20:
 if bateria>=50:
     print("Bateria suficiente para operação.")
 
-#REGRAS DA TEMPERATURA
+
 if temperatura>40:
     print("Temperatura crítica! Operação suspensa.")
 if temperatura<5:
     print("Frio extremo! Modo de economia ativado.")
 
-#REGRAS DA UMIDADE DO SOLO
+
 if umidade<30:
     print("Solo muito seco. Recomendado iniciar irrigação.")
 if umidade>80:
     print("Solo encharcado! Suspenda irrigação imediatamente.")
 
-#REGRAS DO MODO DE OPERAÇÃO
-if modo== "plantio":
+
+if modo== "PLANTIO":
     print("Iniciando modo PLANTIO...")
-if modo== "colheita":
+if modo== "COLHEITA":
     print("Iniciando modo COLHEITA...")
-if modo== "irrigação":
+if modo== "IRRIGAÇÃO":
     print("Iniciando modo IRRIGAÇÃO...")
+
 
 if bateria >= 50:
     if temperatura >= 10:
@@ -38,6 +39,7 @@ if bateria >= 50:
             if umidade >= 30:
                 if umidade <= 80:
                     print("Robô autorizado a iniciar a operação!")
+
 
 if bateria < 50:
     print("Operação negada! Verifique as condições do ambiente.")
